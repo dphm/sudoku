ad3cc4ecd2a27f948366118d2fd6dc2227b2bf12
quelledanielle <quelledanielle@gmail.com>
2014-03-17 22:37:09 -0400

    Improve get_square

--- sudoku.py
+++ sudoku.py
@@ -2,19 +2,23 @@ from sudokusolver import SudokuSolver
 
 class Sudoku(object):
     """3x3 Sudoku puzzle with the following format:
-           | 0 1 2 | 3 4 5 | 6 7 8
-         --|-------|-------|------
-         0 | _ _ _ | _ _ _ | _ _ _
-         1 | _ _ _ | _ _ _ | _ _ _
-         2 | _ _ _ | _ _ _ | _ _ _
-         --|-------|-------|------
-         3 | _ _ _ | _ _ _ | _ _ _
-         4 | _ _ _ | _ _ _ | _ _ _
-         5 | _ _ _ | _ _ _ | _ _ _
-         --|-------|-------|------
-         6 | _ _ _ | _ _ _ | _ _ _
-         7 | _ _ _ | _ _ _ | _ _ _
-         8 | _ _ _ | _ _ _ | _ _ _
+     |--------|-------|-------|-------|-------|
+     | Square |       |   0   |   1   |   2   |
+     |--------|-------|-------|-------|-------|
+     |        | Entry | 0 1 2 | 3 4 5 | 6 7 8 |
+     |--------|-------|-------|-------|-------|
+     |        |   0   | _ _ _ | _ _ _ | _ _ _ |
+     |   0    |   1   | _ _ _ | _ _ _ | _ _ _ |
+     |        |   2   | _ _ _ | _ _ _ | _ _ _ |
+     |--------|-------|-------|-------|-------|
+     |        |   3   | _ _ _ | _ _ _ | _ _ _ |
+     |   1    |   4   | _ _ _ | _ _ _ | _ _ _ |
+     |        |   5   | _ _ _ | _ _ _ | _ _ _ |
+     |--------|-------|-------|-------|-------|
+     |        |   6   | _ _ _ | _ _ _ | _ _ _ |
+     |   2    |   7   | _ _ _ | _ _ _ | _ _ _ |
+     |        |   8   | _ _ _ | _ _ _ | _ _ _ |
+     |--------|-------|-------|-------|-------|
        
        Blank entries have a value of 0.
        Valid entries have a value between 1-9.
@@ -22,7 +26,7 @@ class Sudoku(object):
     
     def __init__(self, filename):
         self.__puzzle = None
-        self.__squares = []
+        self.__square_coords = [(i, j) for i in range(3) for j in range(3)]
         self.__read(filename)
     
     def __str__(self):
@@ -62,20 +66,9 @@ class Sudoku(object):
             column.append(row[j])
         return column
     
-    def get_square(self, i):
-        self.__update_squares()
-        return self.__squares[i]
-        
-    def __update_squares(self):
-        squares = []
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                square = []
-                for row in self.__puzzle[i:i+3]:
-                    for entry in row[j:j+3]:
-                        square.append(entry)
-                squares.append(square)
-        self.__squares = squares
+    def get_square(self, i, j):
+        return map(lambda (x, y): self.get_entry(x + 3 * i, y + 3 * j),
+                   self.__square_coords)
 
     def get_puzzle(self):
         return list(self.__puzzle)
